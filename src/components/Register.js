dc307c08cacd5e8569d7e916a9a947178e8b07e2
singhrupa605 <singhrupa605@gmail.com>
2022-04-20 22:10:21 +0530

    “ME_QKART_FRONTEND_V2_MODULE_REGISTER”  (Crio.Do)

--- src/components/Register.js
+++ src/components/Register.js
@@ -10,32 +10,74 @@ import "./Register.css";
 
 const Register = () => {
   const { enqueueSnackbar } = useSnackbar();
+  const [isLoading, setLoading] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [pswd, setPassword] = useState("");
+  const [confirmPaswd, setConfirmPassword] = useState("");
+  const userData = {
+    username: userName,
+    password: pswd,
+    confirmPassword: confirmPaswd,
+  };
 
+  // Function to delay registration to illustrate circular progress
+   function timeout(delay) {
+    return new Promise((res) => setTimeout(res, delay));
 
-  // TODO: CRIO_TASK_MODULE_REGISTER - Implement the register function
-  /**
-   * Definition for register handler
-   * - Function to be called when the user clicks on the register button or submits the register form
-   *
-   * @param {{ username: string, password: string, confirmPassword: string }} formData
-   *  Object with values of username, password and confirm password user entered to register
-   *
-   * API endpoint - "POST /auth/register"
-   *
-   * Example for successful response from backend for the API call:
-   * HTTP 201
-   * {
-   *      "success": true,
-   * }
-   *
-   * Example for failed response from backend for the API call:
-   * HTTP 400
-   * {
-   *      "success": false,
-   *      "message": "Username is already taken"
-   * }
-   */
+    // TODO: CRIO_TASK_MODULE_REGISTER - Implement the register function
+    /**
+     * Definition for register handler
+     * - Function to be called when the user clicks on the register button or submits the register form
+     *
+     * @param {{ username: string, password: string, confirmPassword: string }} formData
+     *  Object with values of username, password and confirm password user entered to register
+     *
+     * API endpoint - "POST /auth/register"
+     *
+     * Example for successful response from backend for the API call:
+     * HTTP 201
+     * {
+     *      "success": true,
+     * }
+     *
+     * Example for failed response from backend for the API call:
+     * HTTP 400
+     * {
+     *      "success": false,
+     *      "message": "Username is already taken"
+     * }
+     */
+  }
   const register = async (formData) => {
+    setLoading(true);
+    // await timeout(5000);
+    const resData = await axios
+      .post(`${config.endpoint}/auth/register`, {
+        username: formData.username,
+        password: formData.password,
+      })
+      .then((response) => {
+        setLoading(false);
+        console.log(response.status);
+        enqueueSnackbar("Registered Successfully", {
+          variant: "success",
+        });
+      })
+      .catch((error) => {
+        setLoading(false);
+        if (error.response.status === 400) {
+          enqueueSnackbar("Username is already taken", {
+            variant: "error",
+          });
+        } else {
+          enqueueSnackbar(
+            "Something went wrong. Check that the backend is running, reachable and returns valid JSON",
+            {
+              variant: "error",
+            }
+          );
+        }
+      });
   };
 
   // TODO: CRIO_TASK_MODULE_REGISTER - Implement user input validation logic
@@ -57,8 +99,34 @@ const Register = () => {
    * -    Check that confirmPassword field has the same value as password field - Passwords do not match
    */
   const validateInput = (data) => {
+    if (data.username.length === 0) {
+      enqueueSnackbar("Username is a required field", {
+        variant: "error",
+      });
+      return false;
+    } else if (data.username.length < 6) {
+      enqueueSnackbar("Username must be at least 6 characters", {
+        variant: "warning",
+      });
+      return false;
+    } else if (data.password.length === 0) {
+      enqueueSnackbar("Password is a required field", {
+        variant: "error",
+      });
+      return false;
+    } else if (data.password.length < 6) {
+      enqueueSnackbar("Password must be at least 6 characters", {
+        variant: "warning",
+      });
+      return false;
+    } else if (data.password !== data.confirmPassword) {
+      enqueueSnackbar("Passwords do not match", {
+        variant: "error",
+      });
+      return false;
+    }
+    return true;
   };
-
   return (
     <Box
       display="flex"
@@ -77,6 +145,9 @@ const Register = () => {
             title="Username"
             name="username"
             placeholder="Enter Username"
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
             fullWidth
           />
           <TextField
@@ -85,6 +156,9 @@ const Register = () => {
             label="Password"
             name="password"
             type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             helperText="Password must be atleast 6 characters length"
             fullWidth
             placeholder="Enter a password with minimum 6 characters"
@@ -95,16 +169,31 @@ const Register = () => {
             label="Confirm Password"
             name="confirmPassword"
             type="password"
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+            }}
             fullWidth
           />
-           <Button className="button" variant="contained">
+          <Button
+            className="button"
+            variant="contained"
+            onClick={() => {
+              if (validateInput(userData)) {
+                register(userData);
+                console.log(userData);
+              }
+            }}
+          >
             Register Now
-           </Button>
+          </Button>
+          {isLoading && (
+            <CircularProgress color="inherit" style={{ alignSelf: "center" }} />
+          )}
           <p className="secondary-action">
             Already have an account?{" "}
-             <a className="link" href="#">
+            <a className="link" href="#">
               Login here
-             </a>
+            </a>
           </p>
         </Stack>
       </Box>
